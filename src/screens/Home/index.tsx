972574d07eebbc676aaa5ecae378c9bfe174c7dd
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-02-28 23:14:03 -0300

    Add FlatList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Header, TotalCars, HeaderContent } from "./styles";
+import { Container, Header, TotalCars, HeaderContent, CarList } from "./styles";
 
 import Logo from "../../assets/logo.svg";
 import { Car } from "../../components/Car";
 
 export function Home() {
-  const carDataOne = {
+  const carData = {
     brand: "Audi",
     name: "RS 5 Coupé",
     rent: {
@@ -17,26 +17,6 @@ export function Home() {
     },
     thumbnail:
       "https://platform.cstatic-images.com/xlarge/in/v2/stock_photos/b810e10e-50cd-407d-8707-e5b5f8d4e0ea/10aefd6f-af69-4943-8d0c-66d27aaa0feb.png",
-  };
-  const carDataTwo = {
-    brand: "Porsche",
-    name: "Panamera",
-    rent: {
-      period: "ao dia",
-      price: 340,
-    },
-    thumbnail:
-      "https://crdms.images.consumerreports.org/c_lfill,w_720,q_auto,f_auto/prod/cars/cr/model-years/15635-2023-porsche-panamera",
-  };
-  const carDataThree = {
-    brand: "Chevrolet",
-    name: "Corvette z06",
-    rent: {
-      period: "ao dia",
-      price: 620,
-    },
-    thumbnail:
-      "https://inv.assets.sincrod.com/ChromeColorMatch/us/TRANSPARENT_cc_2023CHC060008_01_1280_GD0.png",
   };
 
   return (
@@ -52,9 +32,11 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
-      <Car data={carDataOne} />
-      <Car data={carDataTwo} />
-      <Car data={carDataThree} />
+      <CarList
+        data={[1, 2, 3, 4, 5, 6, 7, 8, 9]}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => <Car data={carData} />}
+      />
     </Container>
   );
 }
