f0b835c40b301528c60eb56a19b49d21e6013044
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-01 22:07:49 -0300

    Add imageSlider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 import { BackButton } from "../../components/BackButton";
-import { Container, Header } from "./style";
+import { ImageSlider } from "../../components/ImageSlider";
+
+import { Container, Header, CarImage } from "./style";
 
 export function CarDetails() {
   return (
@@ -9,6 +11,13 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+      <CarImage>
+        <ImageSlider
+          imageUrl={[
+            "https://platform.cstatic-images.com/xlarge/in/v2/stock_photos/b810e10e-50cd-407d-8707-e5b5f8d4e0ea/10aefd6f-af69-4943-8d0c-66d27aaa0feb.png",
+          ]}
+        />
+      </CarImage>
     </Container>
   );
 }
--- src/screens/CarDetails/style.ts
+++ src/screens/CarDetails/style.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components/native";
 
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.background_secondary};
@@ -9,4 +11,12 @@ export const Header = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+
+  position: absolute;
+  margin-top: ${getStatusBarHeight() + 18}px;
+  margin-left: 24px;
+`;
+
+export const CarImage = styled.View`
+  margin-top: ${getStatusBarHeight() + 32}px;
 `;
