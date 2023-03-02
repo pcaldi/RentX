7871afce7ef6376287f67164929ae9f2f7a0b065
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-02 20:37:12 -0300

    Create component Button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Container, Title } from "./styles";
+
+type ButtonProps = {
+  title: string;
+  color?: string;
+};
+
+export function Button({ title, color, ...rest }: ButtonProps) {
+  return (
+    <Container {...rest} color={color}>
+      <Title>{title}</Title>
+    </Container>
+  );
+}
