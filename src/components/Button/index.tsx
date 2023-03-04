909ff9efd3c93dc1139018b86f9d7b5cb9c239a2
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-04 14:03:09 -0300

    refix onPress button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { useTheme } from "styled-components";
 
 import { Container, Title } from "./styles";
 
 type ButtonProps = {
   title: string;
   color?: string;
+  onPress: () => void;
 };
 
-export function Button({ title, color, ...rest }: ButtonProps) {
+export function Button({ title, color, onPress }: ButtonProps) {
+  const theme = useTheme();
   return (
-    <Container {...rest} color={color}>
+    <Container color={color ? color : theme.colors.main} onPress={onPress}>
       <Title>{title}</Title>
     </Container>
   );
