078682ffce12e7b4a078f8099b5301c2c8dfaaeb
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-08 18:30:45 -0300

    create localeConfig

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,54 +1,34 @@
 import React from "react";
-import { Feather } from "@expo/vector-icons";
 import {
   Calendar as CustomCalendar,
   LocaleConfig,
+  CalendarProps,
 } from "react-native-calendars";
+import { generateInterval } from "./generateInterval";
+import { Feather } from "@expo/vector-icons";
 import { useTheme } from "styled-components";
+import { ptBR } from "./localeConfig";
 
-LocaleConfig.locales["pt-BR"] = {
-  monthNames: [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ],
-  monthNamesShort: [
-    "Jan",
-    "Fev",
-    "Mar",
-    "Abr",
-    "Jun",
-    "Jul",
-    "Ago",
-    "Set",
-    "Out",
-    "Nov",
-    "Dez",
-  ],
-  dayNames: [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado",
-  ],
-  dayNamesShort: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"],
-  today: "Hoje",
-};
-LocaleConfig.defaultLocale = "pt-BR";
+LocaleConfig.locales["pt-br"] = ptBR;
+LocaleConfig.defaultLocale = "pt-br";
 
-export function Calendar() {
+interface MarkedDateProps {
+  [date: string]: {
+    color: string;
+    textColor: string;
+    disabled?: boolean;
+    disableTouchEvent?: boolean;
+  };
+}
+interface DayProps {
+  dateString: string;
+  day: number;
+  month: number;
+  year: number;
+  timestamp: number;
+}
+
+function Calendar({ markedDates, onDayPress }: CalendarProps) {
   const theme = useTheme();
   return (
     <CustomCalendar
@@ -56,29 +36,35 @@ export function Calendar() {
         <Feather
           size={24}
           color={theme.colors.text}
-          name={direction === "left" ? "chevron-left" : "chevron-right"}
+          name={direction == "left" ? "chevron-left" : "chevron-right"}
         />
       )}
       headerStyle={{
         backgroundColor: theme.colors.background_secondary,
         borderBottomWidth: 0.5,
-        borderColor: theme.colors.text_detail,
+        borderBottomColor: theme.colors.text_detail,
         paddingBottom: 10,
         marginBottom: 10,
       }}
       theme={{
         textDayFontFamily: theme.fonts.primary_400,
         textDayHeaderFontFamily: theme.fonts.primary_500,
-        textDayFontSize: 12,
-        textMonthFontSize: 20,
+        textDayHeaderFontSize: 10,
         textMonthFontFamily: theme.fonts.secondary_600,
+        textMonthFontSize: 20,
+        textDayFontSize: 15,
         monthTextColor: theme.colors.title,
         arrowStyle: {
           marginHorizontal: -15,
         },
       }}
       firstDay={1}
-      minDate={String(new Date())}
+      minDate={new Date().toString()}
+      markingType="period"
+      markedDates={markedDates}
+      onDayPress={onDayPress}
     />
   );
 }
+
+export { Calendar, MarkedDateProps, DayProps, generateInterval };
