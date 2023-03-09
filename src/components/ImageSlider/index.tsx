ccf25cb24903373d9976567f8593e28d02f64d6d
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-09 15:46:42 -0300

    Increment function slider in component

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { FlatList, ViewToken } from "react-native";
 
 import {
   Container,
@@ -12,18 +13,39 @@ type CarImageProps = {
   imageUrl: string[];
 };
 
+type ChangedImageProps = {
+  viewableItems: ViewToken[];
+  changed: ViewToken[];
+};
+
 export function ImageSlider({ imageUrl }: CarImageProps) {
+  const [indexImage, setIndexImage] = useState(0);
+
+  const indexChanged = useRef((info: ChangedImageProps) => {
+    const index = info.viewableItems[0].index;
+    setIndexImage(index);
+  });
+
   return (
     <Container>
       <ImageIndexes>
-        <ImageIndex active={true} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
-        <ImageIndex active={false} />
+        {imageUrl.map((_, index) => (
+          <ImageIndex active={index === indexImage} key={String(index)} />
+        ))}
       </ImageIndexes>
-      <CarImageWrapper>
-        <CarImage source={{ uri: imageUrl[0] }} resizeMode="contain" />
-      </CarImageWrapper>
+
+      <FlatList
+        data={imageUrl}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CarImageWrapper>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
+          </CarImageWrapper>
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        onViewableItemsChanged={indexChanged.current}
+      />
     </Container>
   );
 }
