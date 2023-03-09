6fdef58780649fb0dc750477d7bd1ba6317e9492
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-09 15:43:17 -0300

    Create component Mycars

---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { StatusBar, FlatList } from "react-native";
+import { BackButton } from "../../components/BackButton";
+import { CarDTO } from "../../dtos/CarDto";
+import { api } from "../../services/api";
+import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
+import { Car } from "../../components/Car";
+import { Load } from "../../components/Load";
+
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
+} from "./styles";
+
+type MyCarsProps = {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+  startDate: string;
+  endDate: string;
+};
+
+export function MyCars() {
+  const [cars, setCars] = useState<MyCarsProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get("/schedules_byuser?user_id=2");
+        setCars(response.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCars();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+        <BackButton onPress={handleBack} />
+        <Title>
+          Seus agendamentos, {"\n"}
+          estão aqui.
+        </Title>
+        <SubTitle>Conforto, segurança e praticidade.</SubTitle>
+      </Header>
+      {loading ? (
+        <Load />
+      ) : (
+        <Content>
+          <Appointments>
+            <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+            <AppointmentsQuantity>{cars.length}</AppointmentsQuantity>
+          </Appointments>
+          <FlatList
+            data={cars}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <CarWrapper>
+                <Car data={item.car} />
+                <CarFooter>
+                  <CarFooterTitle>PERÍODO</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+                    <AntDesign
+                      name="arrowright"
+                      size={20}
+                      color={theme.colors.text_detail}
+                      style={{ marginHorizontal: 10 }}
+                    />
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWrapper>
+            )}
+          />
+        </Content>
+      )}
+    </Container>
+  );
+}
