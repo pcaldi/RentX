7b727a50fa890e99e759fac3d01c9891c5e94ff8
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-09 15:42:31 -0300

    include enable,loading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ActivityIndicator } from "react-native";
 import { useTheme } from "styled-components";
 
 import { Container, Title } from "./styles";
@@ -7,13 +8,30 @@ type ButtonProps = {
   title: string;
   color?: string;
   onPress: () => void;
+  enable?: boolean;
+  loading?: boolean;
 };
 
-export function Button({ title, color, onPress }: ButtonProps) {
+export function Button({
+  title,
+  color,
+  onPress,
+  enable = true,
+  loading = false,
+}: ButtonProps) {
   const theme = useTheme();
   return (
-    <Container color={color ? color : theme.colors.main} onPress={onPress}>
-      <Title>{title}</Title>
+    <Container
+      color={color ? color : theme.colors.main}
+      onPress={onPress}
+      enabled={enable}
+      style={{ opacity: enable === false || loading === true ? 0.5 : 1 }}
+    >
+      {loading ? (
+        <ActivityIndicator color={theme.colors.shape} />
+      ) : (
+        <Title>{title}</Title>
+      )}
     </Container>
   );
 }
