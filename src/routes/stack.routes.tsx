6a565ab0e9df39361d136e68f037dc70df132a98
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-13 15:31:00 -0300

    Add route splash

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { MyCars } from "../screens/MyCars";
+import { Splash } from "../screens/Splash";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -14,10 +15,18 @@ export function StackRoutes() {
   return (
     <Navigator
       screenOptions={{
-        headerShown: false,
+        headerShown: false, // Retira o header da tela
       }}
+      initialRouteName={"Splash"} // Define qual screen inicial.
     >
-      <Screen name="Home" component={Home} />
+      <Screen name="Splash" component={Splash} />
+      <Screen
+        name="Home"
+        component={Home}
+        options={{
+          gestureEnabled: false, // desabilita o retorno a tela anterior no ios por gestos
+        }}
+      />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
