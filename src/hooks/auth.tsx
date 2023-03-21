4f06b360fc8300c83a00bc8f48fad13fa4e5c0bd
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-21 15:24:26 -0300

    implemented watermelon hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,17 +1,22 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+  useEffect,
+} from "react";
 import { api } from "../services/api";
+import { database } from "../database";
+import { User as ModelUser } from "../database/model/User";
 
 type UserProps = {
   id: string;
+  user_id: string;
   email: string;
   name: string;
   driver_license: string;
   avatar: string;
-};
-// Estado de autenticação. Vou armazenar no estado.
-type AuthState = {
   token: string;
-  user: UserProps;
 };
 
 type SignInCredentials = {
@@ -33,21 +38,52 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 // AuthProvider vai receber um filho, no caso as rotas da aplicação.
 function AuthProvider({ children }: AuthProviderProps) {
-  const [data, setData] = useState<AuthState>({} as AuthState);
+  const [data, setData] = useState<UserProps>({} as UserProps);
 
   async function signIn({ email, password }: SignInCredentials) {
-    const response = await api.post("/sessions", { email, password });
+    try {
+      const response = await api.post("/sessions", { email, password });
 
-    const { token, user } = response.data;
+      const { token, user } = response.data;
+      api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+      const userCollection = database.get<ModelUser>("users");
+      await database.action(async () => {
+        userCollection.create((newUser) => {
+          newUser.user_id = user.id;
+          newUser.name = user.name;
+          newUser.email = user.email;
+          newUser.driver_license = user.driver_license;
+          newUser.avatar = user.avatar;
+          newUser.token = token;
+        });
+      });
 
-    setData({ token, user });
+      setData({ ...user, token });
+    } catch (error) {
+      throw new Error(error);
+    }
   }
+
+  useEffect(() => {
+    async function loadUserData() {
+      const userCollection = await database.get<ModelUser>("users");
+      const response = await userCollection.query().fetch();
+
+      if (response.length > 0) {
+        const userData = response[0]._raw as unknown as ModelUser;
+        api.defaults.headers.common[
+          "Authorization"
+        ] = `Bearer ${userData.token}`;
+        setData(userData);
+      }
+    }
+    loadUserData();
+  });
   return (
     <AuthContext.Provider
       value={{
-        user: data.user,
+        user: data,
         signIn,
       }}
     >
