d57db92a4f2b36b609077e883139af7e7748d998
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-26 15:45:37 -0300

    Add functions

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -3,15 +3,22 @@ import {
   KeyboardAvoidingView,
   Keyboard,
   TouchableWithoutFeedback,
+  Alert,
+  StatusBar,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useNetInfo } from "@react-native-community/netinfo";
 import { useTheme } from "styled-components";
 import { Feather } from "@expo/vector-icons";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import * as ImagePicker from "expo-image-picker";
+import * as Yup from "yup";
+
 import { useAuth } from "../../hooks/auth";
 
 import { BackButton } from "../../components/BackButton";
 import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
 import { Button } from "../../components/Button";
 
 import {
@@ -29,28 +36,101 @@ import {
   OptionTitle,
   Section,
 } from "./styles";
-import { PasswordInput } from "../../components/PasswordInput";
 
 export function Profile() {
+  const { user, signOut, updatedUser } = useAuth();
+  const netInfo = useNetInfo();
+
   const [option, setOption] = useState<"dataEdit" | "passwordEdit">("dataEdit");
+  const [avatar, setAvatar] = useState(user.avatar);
+  const [name, setName] = useState(user.name);
+  const [driverLicense, setDriverLicense] = useState(user.driver_license);
+
   const theme = useTheme();
   const navigation = useNavigation();
-  const { user } = useAuth();
 
   function handleBack() {
     navigation.goBack();
   }
 
-  function handleSignOut() {}
+  async function handleSignOut() {
+    Alert.alert(
+      "Atenção!",
+      "Se você sair, irá precisar de internet para se logar novamente.",
+      [
+        {
+          text: "Cancelar",
+          onPress: () => {},
+          style: "cancel",
+        },
+        {
+          text: "Confirmar",
+          onPress: () => signOut(),
+        },
+      ]
+    );
+  }
 
-  function handleOptionEdit(selected: "dataEdit" | "passwordEdit") {
-    setOption(selected);
+  function handleOptionChange(optionSelected: "dataEdit" | "passwordEdit") {
+    if (netInfo.isConnected === false && optionSelected === "passwordEdit") {
+      Alert.alert(
+        "Você está Offline!",
+        "Você precisa estar conectado para trocar a senha."
+      );
+    } else {
+      setOption(optionSelected);
+    }
+  }
+
+  async function handleAvatarSelect() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setAvatar(result.assets[0].uri);
+    }
+  }
+
+  async function handleProfileUpdate() {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required("Nome é obrigatório."),
+        driverLicense: Yup.string().required("CNH é obrigatória."),
+      });
+      const data = { name, driverLicense };
+      await schema.validate(data);
+
+      await updatedUser({
+        id: user.id,
+        user_id: user.user_id,
+        email: user.email,
+        name,
+        driver_license: user.driver_license,
+        avatar,
+        token: user.token,
+      });
+      Alert.alert("Perfil Atualizado!");
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        Alert.alert("Error: " + error.message);
+      } else {
+        Alert.alert("Não foi possível atualizar o perfil.");
+      }
+    }
   }
 
   return (
     <KeyboardAvoidingView behavior="position" enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <Container>
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent
+          />
           <Header>
             <HeaderTop>
               <BackButton color={theme.colors.shape} onPress={handleBack} />
@@ -60,26 +140,26 @@ export function Profile() {
               </LogoutButton>
             </HeaderTop>
             <PhotoContainer>
-              <Photo source={{ uri: "https://github.com/pcaldi.png" }} />
-              <PhotoButton onPress={() => {}}>
+              {!!avatar && <Photo source={{ uri: avatar }} />}
+              <PhotoButton onPress={handleAvatarSelect}>
                 <Feather name="camera" size={24} color={theme.colors.shape} />
               </PhotoButton>
             </PhotoContainer>
           </Header>
-          {/* Utilizando o useBottomTabBarHeight() faz com que apareça o ultimo
+          {/* Utilizando o useBottomTabBarHeight() fica visível o último
             input junto ao teclado.  */}
           <Content style={{ marginBottom: useBottomTabBarHeight() }}>
             <Options>
               <Option
                 active={option === "dataEdit"}
-                onPress={() => handleOptionEdit("dataEdit")}
+                onPress={() => handleOptionChange("dataEdit")}
               >
                 <OptionTitle active={option === "dataEdit"}>Dados</OptionTitle>
               </Option>
 
               <Option
                 active={option === "passwordEdit"}
-                onPress={() => handleOptionEdit("passwordEdit")}
+                onPress={() => handleOptionChange("passwordEdit")}
               >
                 <OptionTitle active={option === "passwordEdit"}>
                   Trocar senha
@@ -93,6 +173,7 @@ export function Profile() {
                   placeholder="Nome"
                   defaultValue={user.name}
                   autoCorrect={false}
+                  onChangeText={setName}
                 />
                 <Input
                   iconName="mail"
@@ -104,6 +185,7 @@ export function Profile() {
                   placeholder="CNH"
                   defaultValue={user.driver_license}
                   keyboardType="numeric"
+                  onChangeText={setDriverLicense}
                 />
               </Section>
             ) : (
@@ -113,7 +195,7 @@ export function Profile() {
                 <PasswordInput iconName="lock" placeholder="Repetir senha" />
               </Section>
             )}
-            {/*  <Button title="Salvar alterações" /> */}
+            <Button title="Salvar alterações" onPress={handleProfileUpdate} />
           </Content>
         </Container>
       </TouchableWithoutFeedback>
