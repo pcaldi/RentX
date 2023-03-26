b45dbc9437ed40b0ac782eeb0a3d87b91ee3bce4
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-26 15:41:48 -0300

    fix svg icon

--- src/components/AccessoryCar/index.tsx
+++ src/components/AccessoryCar/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTheme } from "styled-components";
 
 import { SvgProps } from "react-native-svg";
 
@@ -10,9 +11,10 @@ type AccessoryCarProps = {
 };
 
 export function AccessoryCar({ name, icon: Icon }: AccessoryCarProps) {
+  const theme = useTheme();
   return (
     <Container>
-      <Icon width={32} height={32} />
+      <Icon width={32} height={32} color={theme.colors.header} />
       <Name>{name}</Name>
     </Container>
   );
