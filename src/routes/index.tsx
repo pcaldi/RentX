89dfd3769c730e00ed64d490f4eb45a514ee1fe6
Paulo Ricardo Caldi <pcaldi@gmail.com>
2023-03-26 15:41:22 -0300

    Add LoadAnimated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,13 @@ import { useAuth } from "../hooks/auth";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppTabRoutes } from "./app.tab.routes";
+import { LoadAnimated } from "../components/LoadAnimated";
 
 export function Routes() {
-  const { user } = useAuth();
-  return (
+  const { user, loading } = useAuth();
+  return loading ? (
+    <LoadAnimated />
+  ) : (
     <NavigationContainer>
       {user.id ? <AppTabRoutes /> : <AuthRoutes />}
     </NavigationContainer>
